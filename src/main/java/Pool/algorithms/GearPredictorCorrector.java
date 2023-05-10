7afb9fb5b005c20464b4c016e52263956bc31fc9
bsquillari <bsquillari@itba.edu.ar>
2023-05-10 15:46:14 -0300

    add: New particles classes

--- src/main/java/Pool/algorithms/GearPredictorCorrector.java
+++ src/main/java/Pool/algorithms/GearPredictorCorrector.java
@@ -1,103 +1,20 @@
 package Pool.algorithms;
 
-import DampedOscillator.models.OscillatorParticle;
+import Pool.models.particle.Particle;
 
-public class GearPredictorCorrector {
+public class GearPredictorCorrector implements DynamicsAlgorithm {
 
-    // Algorithm information
-    private double r0p;
-    private double r1p;
-    private double r2p;
-    private double r3p;
-    private double r4p;
-    private double r5p;
-    private double r0c;
-    private double r1c;
-    private double r2c;
-    private double r3c;
-    private double r4c;
-    private double r5c;
-    private final double dt;
-    private final double[] alphas = {3.0/16, 251.0/360, 1, 11.0/18, 1.0/6, 1.0/60};
-
-    private boolean initialStep = true;
-    // oscillator information
-    private final double k;
-    private final double gamma;
-
-
-
-    private final OscillatorParticle particle;
-
-    public GearPredictorCorrector(double mass, double x, double v, double dt, double k, double gamma){
-        this.dt = dt;
-
-        this.k = k;
-        this.gamma = gamma;
-
-        particle = new OscillatorParticle(x, v, (-k * x - gamma * v)/mass , mass);
-
-        // Initial values
-        r0p = particle.getX();
-        r1p = particle.getV();
-        r2p = particle.getA();
-        r3p = -(k / particle.getMass()) * r1p - (gamma / particle.getMass()) * r2p;
-        r4p = -(k / particle.getMass()) * r2p - (gamma / particle.getMass()) * r3p;
-        r5p = -(k / particle.getMass()) * r3p - (gamma / particle.getMass()) * r4p;
+    public GearPredictorCorrector(Double dt) {
 
     }
 
-    public OscillatorParticle calculateNext() {
+    public void calculateNext() {
 
-        if (!initialStep)
-            predict();
-        double deltaR2 = getDeltaR2();
-        correct(deltaR2);
-
-        particle.setX(r0c);
-        particle.setV(r1c);
-        particle.setA(r2c);
-        initialStep = false;
-
-        return particle;
     }
 
-    private void predict(){
-        r0p = r0c + r1c*dt + r2c*(Math.pow(dt,2)/2) + r3c*(Math.pow(dt,3)/factorial(3)) +
-                r4c*(Math.pow(dt,4)/factorial(4)) + r5c*(Math.pow(dt,5)/factorial(5));
-        r1p = r1c + r2c*dt + r3c*(Math.pow(dt,2)/2) +
-                r4c*(Math.pow(dt,3)/factorial(3)) + r5c*(Math.pow(dt,4)/factorial(4));
-        r2p = r2c + r3c*dt + r4c*(Math.pow(dt,2)/2) + r5c*(Math.pow(dt,3)/factorial(3));
-        r3p = r3c + r4c*dt + r5c*(Math.pow(dt,2)/2);
-        r4p = r4c + r5c*dt;
-        r5p = r5c;
+    @Override
+    public void setParticle(Particle particle) {
+
     }
 
-    private double getDeltaR2(){
-        double a = (-k * r0p - gamma * r1p)/ particle.getMass();
-        double deltaA = a - r2p;
-
-        return deltaA * (Math.pow(dt,2)/2);
-    }
-
-    private void correct(double deltaR2){
-        r0c = r0p + alphas[0]*deltaR2;
-        r1c = r1p + alphas[1]*deltaR2*(1/dt);
-        r2c = r2p + alphas[2]*deltaR2*(2/Math.pow(dt,2));
-        r3c = r3p + alphas[3]*deltaR2*(factorial(3)/Math.pow(dt,3));
-        r4c = r4p + alphas[4]*deltaR2*(factorial(4)/Math.pow(dt,4));
-        r5c = r5p + alphas[5]*deltaR2*(factorial(5)/Math.pow(dt,5));
-    }
-
-    private int factorial(int n) {
-        if (n == 0) {
-            return 1;
-        } else {
-            return n * factorial(n - 1);
-        }
-    }
-
-    public OscillatorParticle getParticle(){
-        return particle;
-    }
 }
---
+++ src/main/java/Pool/models/particle/Particle.java
@@ -0,0 +1,115 @@
+package Pool.models.particle;
+
+import Pool.algorithms.DynamicsAlgorithm;
+import Pool.models.Pair;
+
+import java.util.List;
+
+public class Particle {
+    private static final Double MAX_X = 224.0;
+    private static final Double MAX_Y = 112.0;
+    private static final Double K = 10000.0;
+    private final Pair<Double> position;
+    private final Pair<Double> velocity;
+    private final Double radius;
+    private final Double mass;
+    private final DynamicsAlgorithm algorithm;
+    private Pair<Double> force;
+
+    public Particle(Double x, Double y, Double vx, Double vy, Double radius, Double mass, DynamicsAlgorithm algorithm) {
+        this.position = new Pair<>(x, y);
+        this.velocity = new Pair<>(vx, vy);
+        this.radius = radius;
+        this.mass = mass;
+        this.algorithm = algorithm;
+        this.algorithm.setParticle(this);
+    }
+
+
+    public void move(List<Particle> particles) {
+
+        this.updateForce(particles);
+        this.algorithm.calculateNext();
+
+    }
+
+    private void updateForce(List<Particle> particles) {
+        this.force.setX(0.0);
+        this.force.setY(0.0);
+        particles.forEach(
+                p -> {
+                    if (this.borderDistanceTo(p.position, p.radius) <= 0) {
+                        Pair<Double> interactionForce = this.getInteractionForce(p);
+                        this.force.setX(this.force.getX() + interactionForce.getX());
+                        this.force.setY(this.force.getY() + interactionForce.getY());
+                    }
+                }
+        );
+
+        if (position.getX() < 0)
+            this.force.setX(this.force.getX() + position.getX() * K);
+        if (position.getX() > MAX_X)
+            this.force.setX(this.force.getX() - (position.getX() - MAX_X) * K);
+
+        if (position.getY() < 0)
+            this.force.setY(this.force.getY() + position.getY() * K);
+        if (position.getY() > MAX_Y)
+            this.force.setY(this.force.getY() - (position.getY() - MAX_Y) * K);
+    }
+
+
+    public Pair<Double> getInteractionForce(Particle particle) {
+        return null;
+    }
+
+    public Double getX() {
+        return position.getX();
+    }
+
+
+    public Double getY() {
+        return position.getY();
+    }
+
+
+    public Pair<Double> getPosition() {
+        return position;
+    }
+
+
+    public Double getVx() {
+        return velocity.getX();
+    }
+
+
+    public Double getVy() {
+        return velocity.getY();
+    }
+
+
+    public Double getRadius() {
+        return radius;
+    }
+
+    public Pair<Double> getForce() {
+        return force;
+    }
+
+    public Double borderDistanceTo(Pair<Double> o, Double radius) {
+        return Math.sqrt(
+                Math.pow(getX() - o.getX(), 2) + Math.pow(getY() - o.getY(), 2)
+        ) - (getRadius() + radius);
+    }
+
+    public Pair<Double> getVelocity() {
+        return velocity;
+    }
+
+    public Double getMass() {
+        return mass;
+    }
+
+    public String toString() {
+        return position.getX() + " " + position.getY() + " " + velocity.getX() + " " + velocity.getY() + " " + radius + " 255 0 0";
+    }
+}
